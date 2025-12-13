66ec06f5cb1780f41718b09bf394263c4e07f083
tealslug <tealslug@lionsoft.com>
2025-12-13 00:05:20 -0800

    feat: Add functions to extract markdown images and links, along with their unit tests.

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_on(nodes: list[TextNode], split_on: str, text_type: TextType) -> list[TextNode]:
@@ -16,3 +17,22 @@ def split_nodes_on(nodes: list[TextNode], split_on: str, text_type: TextType) ->
     new_nodes.append(TextNode(parts[2], TextType.PLAIN))
 
   return new_nodes
+
+# Extracts all regex pairs from a string.  By pairs we mean that the pattern
+# should have two groups, and we return a list of those groups.
+def extract_regex_pairs(text: str, regex: re.Pattern) -> list[(str, str)]:
+  found = []
+  while True:
+    match = regex.search(text)
+    if match is None:
+      break
+    found.append(match.groups())
+    text = text[match.end():]
+
+  return found
+
+def extract_markdown_images(text: str) -> list[(str, str)]:
+  return extract_regex_pairs(text, re.compile(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"))
+
+def extract_markdown_links(text: str) -> list[(str, str)]:
+  return extract_regex_pairs(text, re.compile(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"))
--- src/test_processing.py
+++ src/test_processing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from processing import split_nodes_on
+from processing import split_nodes_on, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestProcessing(unittest.TestCase):
@@ -18,3 +18,27 @@ class TestProcessing(unittest.TestCase):
     nodes = [TextNode("one `two` three", TextType.PLAIN)]
     new_nodes = split_nodes_on(nodes, "`", TextType.CODE)
     self.assertEqual(new_nodes, [TextNode("one ", TextType.PLAIN), TextNode("two", TextType.CODE), TextNode(" three", TextType.PLAIN)])
+
+  def test_extract_markdown_images_single(self):
+    text = "![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+    self.assertEqual(extract_markdown_images(text), [("rick roll", "https://i.imgur.com/aKaOqIh.gif")])
+
+  def test_extract_markdown_images_multiple(self):
+    text = "![rick roll](https://i.imgur.com/aKaOqIh.gif)![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    self.assertEqual(extract_markdown_images(text), [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+  def test_extract_markdown_images_none(self):
+    text = "this text doesn't have any images"
+    self.assertEqual(extract_markdown_images(text), [])
+  
+  def test_extract_markdown_links_single(self):
+    text = "This is text with a link [to boot dev](https://www.boot.dev)"
+    self.assertEqual(extract_markdown_links(text), [("to boot dev", "https://www.boot.dev")])
+  
+  def test_extract_markdown_links_multiple(self):
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    self.assertEqual(extract_markdown_links(text), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+  
+  def test_extract_markdown_links_none(self):
+    text = "this text doesn't have any links"
+    self.assertEqual(extract_markdown_links(text), [])
